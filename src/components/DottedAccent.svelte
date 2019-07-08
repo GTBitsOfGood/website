7888f7afd1b41bae41a7ad76bfdfc59008ea53dd
holben888 <bholmesdev@gmail.com>
2019-07-08 09:05:11 -0400

    add dotted accent file

---
+++ src/components/DottedAccent.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let width = "small";
+  export let height = "small";
+
+  const sizes = {
+    1: "small",
+    2: "medium",
+    3: "large"
+  };
+</script>
+
+<style>
+  div {
+    background: radial-gradient(1rem);
+  }
+</style>
+
+<div class="{width} {height}" />
