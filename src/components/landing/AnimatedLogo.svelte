aba97cfdabef9ae641a857b19a18b01d5e598731
holben888 <bholmesdev@gmail.com>
2019-07-12 09:26:57 -0400

    clean up svg markup

--- src/components/landing/AnimatedLogo.svelte
+++ src/components/landing/AnimatedLogo.svelte
@@ -23,8 +23,7 @@
   height="1300"
   viewBox="0 0 1440 1300"
   fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink">
+  xmlns="http://www.w3.org/2000/svg">
 
   <image
     image
@@ -41,7 +40,7 @@
     104.01 552.13 126.478 608.237 210.232C608.237 210.232 665.693 281.794
     673.707 405.672C681.721 529.55 649.325 706.285 524.624 883.028C464.169
     968.711 305.285 1126.2 0 1299.43V1300H1439.5V0Z"
-    fill="url(#paint0_linear)" />
+    fill="url(#backing-gradient)" />
   <path
     d="M521.287 366.324C524.395 426.026 458.384 496.246 393.427 496.245C358.403
     496.245 324.814 482.557 300.049 458.192C275.283 433.827 261.37 400.782
@@ -50,19 +49,14 @@
     601.532 340.948 647.318 483.164 602.346C483.164 602.346 538.443 485.594
     521.287 366.324Z"
     fill="#FEF2EA" />
-  <path d="M262 102H494.334V374.841H262V102Z" fill="url(#paint1_linear)" />
   <defs>
-    <pattern
-      id="pattern0"
-      patternContentUnits="objectBoundingBox"
-      width="1"
-      height="1">
+    <pattern patternContentUnits="objectBoundingBox" width="1" height="1">
       <use
         xlink:href="#logo-backing-img"
         transform="translate(0 -0.00855773) scale(0.00164488 0.000951464)" />
     </pattern>
     <linearGradient
-      id="paint0_linear"
+      id="backing-gradient"
       x1="1439"
       y1="3.96734e-06"
       x2="851.5"
@@ -70,17 +64,6 @@
       gradientUnits="userSpaceOnUse">
       <stop stop-color="#FBF6F2" />
       <stop offset="1" stop-color="white" />
-    </linearGradient>
-    <linearGradient
-      id="paint1_linear"
-      x1="262"
-      y1="238"
-      x2="494.334"
-      y2="238.421"
-      gradientUnits="userSpaceOnUse">
-      <stop stop-color="#FEFCFB" />
-      <stop offset="0.78125" stop-color="#FFFDF5" stop-opacity="0.13" />
-      <stop offset="1" stop-color="#FFFDF5" stop-opacity="0" />
     </linearGradient>
   </defs>
 </svg>
