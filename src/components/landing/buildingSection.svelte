32dd8a39f1b59bc3df05ee370622cebee2749bf3
holben888 <bholmesdev@gmail.com>
2019-07-18 10:16:21 -0400

    create info sections

---
+++ src/components/landing/buildingSection.svelte
@@ -0,0 +1,1 @@
+<h2>Building powerful web apps</h2>
---
+++ src/components/landing/connectingSection.svelte
@@ -0,0 +1,21 @@
+<script>
+  import SplitSection from '../SplitSection.svelte'
+</script>
+
+<style>
+  a {
+    font-size: 24px;
+  }
+</style>
+
+<SplitSection>
+  <div slot="left">
+    <h2>Connecting with ATL</h2>
+    <p>
+      By building relationships with local nonprofits, we are redefining social
+      good for students who want to make a meaningful impact with their
+      technical background.
+    </p>
+    <a href="about-us">More about our mission</a>
+  </div>
+</SplitSection>
