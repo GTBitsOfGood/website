82941dd65fb109232f179ae15204fee7e5def3d4
Holben888 <holben888@gmail.com>
2019-07-20 16:28:00 -0400

    fix split section layout for narrow screens

--- src/components/InfoSplitSection.svelte
+++ src/components/InfoSplitSection.svelte
@@ -2,23 +2,29 @@
   section {
     display: flex;
     position: relative;
-    flex-wrap: wrap;
+    flex-wrap: wrap-reverse;
     min-height: 60rem;
     margin: 0 auto;
     margin-bottom: 10rem;
-    padding: 0 6rem;
     max-width: 130rem;
   }
   .left {
     flex: 1;
-    margin-right: 6rem;
   }
   .right {
     flex: 1.2;
   }
   .left,
   .right {
-    min-width: 40rem;
+    width: 100%;
+    flex-basis: 40rem;
+    margin: 3rem;
+  }
+  @media (max-width: 500px) {
+    .left,
+    .right {
+      margin: 2rem;
+    }
   }
 </style>
 
