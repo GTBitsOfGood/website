22432262a0fca846035430f66a097da86dcb928c
Holben888 <holben888@gmail.com>
2019-07-20 15:40:59 -0400

    update imports to capital file names

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,4 +1,4 @@
-export { default as Heading } from './heading.svelte'
-export { default as ConnectingSection } from './connectingSection.svelte'
-export { default as BuildingSection } from './buildingSection.svelte'
+export { default as Heading } from './Heading.svelte'
+export { default as ConnectingSection } from './ConnectingSection.svelte'
+export { default as BuildingSection } from './BuildingSection.svelte'
 export { default as AnimatedLogo } from './AnimatedLogo.svelte'
