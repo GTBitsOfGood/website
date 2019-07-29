a8e4db0b4f46a1e32eb542d1a383365b40e38d7e
Holben888 <holben888@gmail.com>
2019-07-28 21:31:36 -0400

    add info card component

---
+++ src/components/InfoCard.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let imgSrc = ''
+  export let imgAlt = ''
+  export let header = ''
+  export let body = ''
+
+  export let label = ''
+
+  const labels = ['In Development', 'In Early Design', 'Near finish line']
+  $: validLabel = labels.includes(label) ? label : ''
+</script>
+
+<style>
+  .container {
+    background: white;
+    box-shadow: var(--shadow);
+    border-radius: 1rem;
+    display: inline-block;
+    max-width: 32rem;
+    min-width: 27rem;
+    flex: 1;
+    margin: 2rem;
+  }
+  .img-container {
+    height: 27rem;
+    width: 100%;
+    margin-bottom: 2rem;
+    border-radius: inherit;
+    background: grey;
+    overflow: hidden;
+  }
+  .img-container > img {
+    height: 100%;
+    width: auto;
+  }
+  h3,
+  p {
+    padding: 0 2rem;
+  }
+</style>
+
+<div class="container">
+  <div class="img-container">
+    <img src={imgSrc} alt={imgAlt} />
+  </div>
+  <h3>{header}</h3>
+  <p>{body}</p>
+  {#if validLabel}
+    <small>{validLabel}</small>
+  {/if}
+</div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as InfoSplitSection } from './InfoSplitSection.svelte'
 export { default as GetInvolvedBtn } from './GetInvolvedBtn.svelte'
 export { default as DottedAccent } from './DottedAccent.svelte'
+export { default as InfoCard } from './InfoCard.svelte'
