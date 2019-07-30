c76cf5d264328b429e8461b7e128539124d68497
SujeethJinesh <sujeethjinesh@gmail.com>
2019-07-29 21:50:19 -0700

    Add Google Analytics 📊

--- src/client.js
+++ src/client.js
@@ -1,5 +1,10 @@
 import * as sapper from '@sapper/app';
+import { googleAnalytics } from './js/google-analytics.js';
 
 sapper.start({
 	target: document.querySelector('#sapper')
 });
+
+GOOGLE_ANALYTICS_TRACKING_ID = undefined; // TODO: Add in import for tracking id
+
+googleAnalytics(GOOGLE_ANALYTICS_TRACKING_ID);
