2db526639527a63c1611e1d368c100a8168baa5b
Holben888 <holben888@gmail.com>
2019-07-30 07:46:09 -0400

    adjust card styling and add example labels

--- src/components/InfoCard/index.svelte
+++ src/components/InfoCard/index.svelte
@@ -10,28 +10,30 @@
   export let href = ''
   export let linkText = 'Learn more'
 
-  const labels = ['In Development', 'In Early Design', 'Near finish line']
-  $: validLabel = labels.includes(label) ? label : ''
+  const labels = [
+    {
+      label: 'In Early Design',
+      color: '#FFEECE',
+    },
+    {
+      label: 'In Development',
+      color: '#FFB374',
+    },
+    {
+      label: 'Near finish line',
+      color: '#FF8988',
+    },
+  ]
+  $: validLabel = labels.find(l => l.label === label)
 </script>
 
 <style>
-  .container {
-    background: white;
-    box-shadow: var(--shadow);
-    border-radius: 1rem;
-    position: relative;
-    display: inline-block;
-    max-width: 32rem;
-    min-width: 27rem;
-    flex: 1;
-    margin: 2rem;
-  }
   .img-container {
     height: 27rem;
     width: 100%;
     margin-bottom: 2rem;
     border-radius: inherit;
-    background: grey;
+    background: #333;
     overflow: hidden;
   }
   .img-container > img {
@@ -41,6 +43,18 @@
   h3,
   p {
     padding: 0 2rem;
+    margin: 1rem 0;
+  }
+  p {
+    margin-bottom: 2rem;
+  }
+  small {
+    font-size: 14px;
+    padding: 0.7rem 2rem;
+    border-radius: 3rem;
+    margin: 3rem 2rem;
+    margin-top: auto;
+    width: auto;
   }
 </style>
 
@@ -51,6 +65,8 @@
   <h3>{header}</h3>
   <p>{body}</p>
   {#if validLabel}
-    <small>{validLabel}</small>
+    <small style={`background-color: ${validLabel.color}`}>
+      {validLabel.label}
+    </small>
   {/if}
 </Container>
