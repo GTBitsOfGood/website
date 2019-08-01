ae46b14c17f9b8f4310e2a9949fc7033ac98a076
Benjamin Holmes <bholmesdev@gmail.com>
2019-08-01 07:46:15 -0400

    Add reusable card component for roles and projects (#17)
    * add info card component
    
    * create role list from cards
    
    * update paragraphs to match css globals
    
    * add hyperlink container to info card
    
    * update font size cascade for headers, p
    
    * adjust card container styling
    
    * add projects example cards
    
    * styling fixes, add example data
    
    * adjust card styling and add example labels
    
    * remove unused css variable
    
    * Convert PNG photos to JPEGs (#18)

---
+++ src/components/InfoCard/Container.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let href = ''
+</script>
+
+<style>
+  a,
+  div {
+    position: relative;
+    background: white;
+    box-shadow: var(--shadow);
+    border-radius: 1rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    max-width: 32rem;
+    min-width: 27rem;
+    flex: 1;
+    margin: 2rem;
+  }
+
+  a {
+    font-weight: 500;
+    transition: transform 0.2s, box-shadow 0.2s;
+  }
+  a :global(img) {
+    transition: opacity 0.2s;
+    opacity: 0.8;
+  }
+  a,
+  a:hover {
+    color: initial;
+  }
+  a:hover {
+    transform: scale(1.1);
+    box-shadow: var(--shadow-hover);
+  }
+  a:hover :global(img) {
+    opacity: 1;
+  }
+  .arrow {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    width: 4rem;
+    height: 4rem;
+    z-index: 1;
+  }
+</style>
+
+{#if href}
+  <a {href}>
+    <img class="arrow" src="/icons/arrow-right.svg" alt="" />
+    <slot />
+  </a>
+{:else}
+  <div>
+    <slot />
+  </div>
+{/if}
---
+++ src/components/InfoCard/index.svelte
@@ -0,0 +1,72 @@
+<script>
+  import Container from './Container.svelte'
+
+  export let imgSrc = ''
+  export let imgAlt = ''
+  export let header = ''
+  export let body = ''
+
+  export let label = ''
+  export let href = ''
+  export let linkText = 'Learn more'
+
+  const labels = [
+    {
+      label: 'In Early Design',
+      color: '#FFEECE',
+    },
+    {
+      label: 'In Development',
+      color: '#FFB374',
+    },
+    {
+      label: 'Near finish line',
+      color: '#FF8988',
+    },
+  ]
+  $: validLabel = labels.find(l => l.label === label)
+</script>
+
+<style>
+  .img-container {
+    height: 27rem;
+    width: 100%;
+    margin-bottom: 2rem;
+    border-radius: inherit;
+    background: #333;
+    overflow: hidden;
+  }
+  .img-container > img {
+    height: 100%;
+    width: auto;
+  }
+  h3,
+  p {
+    padding: 0 2rem;
+    margin: 1rem 0;
+  }
+  p {
+    margin-bottom: 2rem;
+  }
+  small {
+    font-size: 14px;
+    padding: 0.7rem 2rem;
+    border-radius: 3rem;
+    margin: 3rem 2rem;
+    margin-top: auto;
+    width: auto;
+  }
+</style>
+
+<Container {href}>
+  <div class="img-container">
+    <img src={imgSrc} alt={imgAlt} />
+  </div>
+  <h3>{header}</h3>
+  <p>{body}</p>
+  {#if validLabel}
+    <small style={`background-color: ${validLabel.color}`}>
+      {validLabel.label}
+    </small>
+  {/if}
+</Container>
