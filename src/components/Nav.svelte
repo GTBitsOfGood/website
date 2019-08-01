7ec4d4fa825428448c421f3aef912b3e3ce223bc
holben888 <bholmesdev@gmail.com>
2019-08-01 09:43:50 -0400

    update nav for mobile layout

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,8 @@
 <script>
   import GetInvolvedBtn from './GetInvolvedBtn.svelte'
+  import MenuIcon from './MenuIcon.svelte'
+
+  let menuToggled = false
 
   export let segment
   export let scrolled = true
@@ -13,6 +16,8 @@
     background: var(--backing-pink-pronounced);
     transition: box-shadow 0.2s;
     z-index: 1;
+    display: flex;
+    align-items: center;
   }
 
   .shadow {
@@ -20,8 +25,9 @@
   }
 
   ul {
-    display: flex;
-    align-items: center;
+    display: inherit;
+    align-items: inherit;
+    flex: 1;
     height: 100%;
     max-width: 140rem;
     margin: auto;
@@ -34,7 +40,7 @@
     color: var(--text-color);
   }
 
-  li:last-of-type {
+  li.align-right {
     margin-left: auto;
     margin-right: 0;
     color: var(--primary);
@@ -44,7 +50,7 @@
     color: inherit;
   }
 
-  li:last-of-type > a {
+  li.align-right > a {
     margin-left: 4rem;
   }
 
@@ -53,9 +59,67 @@
     color: var(--primary);
   }
 
-  li:last-of-type > a:hover,
-  li:last-of-type > a:active {
+  ul:not(.menu-toggled) > li.align-right > a:hover,
+  ul:not(.menu-toggled) > li.align-right > a:active {
     color: var(--primary-yellow);
+  }
+
+  .mobile-content {
+    display: none;
+    align-items: inherit;
+    padding: 0 2rem;
+    flex: 1;
+  }
+
+  .mobile-dropdown-toggle {
+    display: none;
+    margin-left: auto;
+    background: none;
+  }
+
+  @media (max-width: 45rem) {
+    :root {
+      --nav-height: 5rem;
+    }
+    .mobile-content {
+      display: inherit;
+    }
+    .mobile-dropdown-toggle {
+      display: initial;
+    }
+    ul {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      background: var(--backing-pink);
+      transition: height 0.3s, opacity 0.4s;
+      opacity: 0;
+      z-index: -1;
+    }
+    ul.menuToggled {
+      height: 100vh;
+      opacity: 1;
+    }
+    a {
+      font-size: 2rem;
+      margin: 1rem;
+    }
+    li,
+    li.align-right {
+      margin: 2rem;
+    }
+    li.align-right > a {
+      color: var(--text-color);
+    }
+    li.align-right > a:hover,
+    li.align-right > a:active {
+      color: var(--primary);
+    }
   }
 
   .selected {
@@ -64,7 +128,7 @@
 </style>
 
 <nav class:shadow={scrolled}>
-  <ul>
+  <ul class:menuToggled on:click={() => (menuToggled = !menuToggled)}>
     <li>
       <a class:selected={segment === undefined} href=".">Home</a>
     </li>
@@ -74,12 +138,20 @@
     <li>
       <a class:selected={segment === 'projects'} href="projects">Projects</a>
     </li>
-    <li />
-    <li>
-      <GetInvolvedBtn hide={!scrolled && segment === undefined} />
+    <li class="align-right">
+      <GetInvolvedBtn
+        hide={menuToggled || (!scrolled && segment === undefined)} />
       <a class:selected={segment === 'contact-us'} href="contact-us">
         Contact Us
       </a>
     </li>
   </ul>
+  <div class="mobile-content">
+    <GetInvolvedBtn small />
+    <button
+      class="mobile-dropdown-toggle"
+      on:click={() => (menuToggled = !menuToggled)}>
+      <MenuIcon {menuToggled} />
+    </button>
+  </div>
 </nav>
