be4f06a101ac8f07a0a0b55f7d3ae829c1f359c2
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2019-07-31 23:10:52 -0400

    Convert PNG photos to JPEGs

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -5,7 +5,7 @@
     {
       header: 'Bright Futures Academy',
       body: `This is a web app built to manage payroll at Bright Futures Academy in Atlanta. It provides information for prospective students, and enrolled families can sign in to manage their students' tuition and other payments.`,
-      imgSrc: '/example-data/bfa.png',
+      imgSrc: '/example-data/bfa.jpg',
       imgAlt: '',
       href: 'https://github.com/GTBitsOfGood/bright-futures-academy',
       label: 'In Early Design',
@@ -13,7 +13,7 @@
     {
       header: 'Drawchange',
       body: `This portal is meant to be used by Atlanta nonprofit drawchange to manage their volunteers and events. New volunteers will register an account, be vetted by an administrator, then sign up for volunteer events through this portal.`,
-      imgSrc: '/example-data/drawchange.png',
+      imgSrc: '/example-data/drawchange.jpg',
       imgAlt: '',
       href: 'https://github.com/GTBitsOfGood/drawchange',
       label: 'In Development',
@@ -21,7 +21,7 @@
     {
       header: 'International Rescue Committee',
       body: `This inventory system is being developed as a pilot system for the Atlanta office of IRC. It is meant to track inventory usage at their shopfront, and log volunteer hours. Further, it can create reports regarding contributions.`,
-      imgSrc: '/example-data/irc.png',
+      imgSrc: '/example-data/irc.jpg',
       imgAlt: '',
       href: 'https://github.com/GTBitsOfGood/irc',
       label: 'Near finish line',
