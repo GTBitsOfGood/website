0c0534eaa165c6e4c9dee9be94794ccc21732fa0
Holben888 <holben888@gmail.com>
2019-08-19 18:59:03 -0400

    update mission section with contentful content

--- src/components/about-us/MissionSection.svelte
+++ src/components/about-us/MissionSection.svelte
@@ -1,22 +1,50 @@
 <script>
-  import MissionCard from './MissionCard'
+  import content from '@contentful-entries/missionSection'
+  import { toHtml, toImg } from 'contentful-utils'
+  const missionCards = content.map(card => ({
+    ...card.fields,
+    img: toImg(card.fields, 'aboutUsImage'),
+  }))
   export let id = ''
 </script>
 
 <style>
   h2 {
     text-align: center;
-    grid-column: span 2;
+    grid-column: 1 / -1;
   }
   section {
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: repeat(auto-fill, minmax(38rem, 1fr));
+    justify-items: center;
     column-gap: 10rem;
+    row-gap: 5rem;
+    padding: 3rem;
+  }
+
+  .mission-card {
+    max-width: 50rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  h3 {
+    text-align: center;
+    color: var(--primary-yellow);
+  }
+  .mission-card > :global(p) {
+    margin: 0;
+    font-size: 24px;
   }
 </style>
 
 <section {id}>
   <h2>Our Mission</h2>
-  <MissionCard />
-  <MissionCard />
+  {#each missionCards as { heading, description, img }}
+    <div class="mission-card">
+      <img src={img.src} alt={img.alt} />
+      <h3>{heading}</h3>
+      {@html toHtml(description)}
+    </div>
+  {/each}
 </section>
