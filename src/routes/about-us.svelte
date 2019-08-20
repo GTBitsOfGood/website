52192aa93bc03d9b1a0902c6d952c29cfd1f9e61
Holben888 <holben888@gmail.com>
2019-08-20 08:54:18 -0400

    add slogan section to about us

--- src/routes/about-us.svelte
+++ src/routes/about-us.svelte
@@ -2,10 +2,12 @@
   import Roles from '../components/about-us/RolesSection'
   import Header from '../components/about-us/Header'
   import Mission from '../components/about-us/MissionSection'
+  import Slogan from '../components/about-us/Slogan'
 
   const missionSectionId = 'mission-section'
 </script>
 
 <Header {missionSectionId} />
 <Mission id={missionSectionId} />
+<Slogan />
 <Roles />
