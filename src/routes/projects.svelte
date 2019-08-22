de25c008c6b18fd11adf1dda505a613e36f9a9e9
Holben888 <holben888@gmail.com>
2019-08-22 09:46:12 -0400

    import projects from contentful

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,39 +1,15 @@
 <script>
   import InfoCard from '../components/InfoCard/index.svelte'
+  import content from '@contentful-entries/project'
+  import { toHtml, mapFields } from 'contentful-utils'
 
-  const exampleProjects = [
-    {
-      header: 'Bright Futures Academy',
-      body: `This is a web app built to manage payroll at Bright Futures Academy in Atlanta. It provides information for prospective students, and enrolled families can sign in to manage their students' tuition and other payments.`,
-      imgSrc: '/example-data/bfa.jpg',
-      imgAlt: '',
-      href: 'https://github.com/GTBitsOfGood/bright-futures-academy',
-      label: 'In Early Design',
-    },
-    {
-      header: 'Drawchange',
-      body: `This portal is meant to be used by Atlanta nonprofit drawchange to manage their volunteers and events. New volunteers will register an account, be vetted by an administrator, then sign up for volunteer events through this portal.`,
-      imgSrc: '/example-data/drawchange.jpg',
-      imgAlt: '',
-      href: 'https://github.com/GTBitsOfGood/drawchange',
-      label: 'In Development',
-    },
-    {
-      header: 'International Rescue Committee',
-      body: `This inventory system is being developed as a pilot system for the Atlanta office of IRC. It is meant to track inventory usage at their shopfront, and log volunteer hours. Further, it can create reports regarding contributions.`,
-      imgSrc: '/example-data/irc.jpg',
-      imgAlt: '',
-      href: 'https://github.com/GTBitsOfGood/irc',
-      label: 'Near finish line',
-    },
-  ]
+  const projects = mapFields(content, 'thumbnail')
 </script>
 
 <style>
   section {
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
   }
   h2 {
     width: 100%;
@@ -43,7 +19,12 @@
 
 <section>
   <h2>Projects</h2>
-  {#each exampleProjects as project}
-    <InfoCard {...project} />
+  {#each projects as { heading, description, developmentStatus, link, img }}
+    <InfoCard
+      {heading}
+      {link}
+      {img}
+      descriptionHTML={toHtml(description)}
+      label={developmentStatus} />
   {/each}
 </section>
