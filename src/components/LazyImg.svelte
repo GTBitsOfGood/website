ba4f9c9f7946ae97d94a7e9e94733433df2a138f
Holben888 <holben888@gmail.com>
2019-09-07 14:10:56 -0400

    fade placeholder, update callback, update props

--- src/components/LazyImg.svelte
+++ src/components/LazyImg.svelte
@@ -2,43 +2,42 @@
   export let src = ''
   export let placeholder = ''
   export let alt = ''
-  export let imgStyle = ''
-  export let containerStyle = ''
 
   let placeholderImg
 
-  const watch = node => {
-    node.onload = () => {
-      node.setAttribute('style', imgStyle)
+  const watchForImgLoad = fullResImg => {
+    const transitionDuration = 200
+    fullResImg.onload = () => {
+      fullResImg.style.display = 'initial'
+
+      placeholderImg.style.transition = `opacity ${transitionDuration}ms`
       placeholderImg.style.opacity = 0
       setTimeout(() => {
         placeholderImg.remove()
-      }, 0)
+      }, transitionDuration)
     }
   }
 </script>
 
 <style>
-  div.container {
+  .container {
     position: relative;
+    width: 100%;
     height: 100%;
-    width: auto;
+    overflow: hidden;
   }
-  img.lazy {
-    opacity: 1;
-    transition: opacity 0.4s;
+  img {
     position: absolute;
     top: 0;
     left: 0;
+    height: 100%;
+  }
+  img.full-res {
+    display: none;
   }
 </style>
 
-<div class="container" style={containerStyle}>
-  <img
-    class="lazy"
-    bind:this={placeholderImg}
-    src={placeholder}
-    style={imgStyle}
-    {alt} />
-  <img class="lazy" use:watch style="opacity:0;{imgStyle}" {src} {alt} />
+<div class="container">
+  <img use:watchForImgLoad class="full-res" {src} {alt} />
+  <img bind:this={placeholderImg} src={placeholder} {alt} />
 </div>
