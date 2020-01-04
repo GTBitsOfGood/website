f57f26c241f134d2b035ac8fa2973869d6681921
Holben888 <holben888@gmail.com>
2020-01-03 23:12:51 -0500

    move heading to separate component, add nonprofit section

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -1,46 +1,12 @@
 <script>
+  import Heading from '../components/join/Heading.svelte'
   import openRoles from '@contentful-entries/openRole'
   import content from '@contentful-entry/joinPage'
+  import { removeWrapperPTag } from '../contentHelpers'
   console.log(openRoles, content)
 </script>
 
 <style>
-  section.heading {
-    position: relative;
-    background: linear-gradient(
-      130deg,
-      var(--primary-red) 24.55%,
-      var(--primary-yellow) 85.2%
-    );
-    max-width: 100%;
-  }
-
-  hgroup {
-    margin: auto;
-    padding: 50px 0;
-    text-align: center;
-    letter-spacing: 0.2em;
-  }
-  hgroup h1 {
-    font-size: 72px;
-    color: white;
-  }
-  hgroup h2 {
-    font-size: 24px;
-    text-transform: uppercase;
-  }
-
-  p {
-    font-weight: 300;
-  }
-
-  svg.bottom-swoosh-overlay {
-    width: 100%;
-    height: 20vw;
-    max-height: 300px;
-    margin-bottom: -10px;
-  }
-
   section.content {
     padding: 30px;
   }
@@ -77,31 +43,13 @@
   }
 </style>
 
-<section class="heading">
-  <hgroup>
-    <h1>Join Us!</h1>
-    <h2>Spring 2020</h2>
-  </hgroup>
-
-  <svg
-    class="bottom-swoosh-overlay"
-    width="1440"
-    height="255"
-    viewBox="0 0 1440 255"
-    fill="none"
-    preserveAspectRatio="none"
-    xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M1440 255H0V0C77.5 109.112 392 214.819 714 101.388C864 48.5474 1245
-      6.87843 1440 128V255Z"
-      fill="white" />
-  </svg>
-</section>
-
+<Heading />
 <section class="content">
   <section id="students">
     <h2>Students</h2>
-    <p>{content.studentTagline}</p>
+    <p class="tagline">
+      {@html removeWrapperPTag(content.studentTagline)}
+    </p>
     <h3>Open roles</h3>
     {#each openRoles as openRole}
       <a class="open-role" href={openRole.applicationUrl}>
@@ -112,5 +60,12 @@
         <p>{openRole.description}</p>
       </a>
     {/each}
+  </section>
+  <section id="nonprofits">
+    <h2>Nonprofits</h2>
+    <p class="tagline">
+      {@html removeWrapperPTag(content.nonprofitTagline)}
+    </p>
+    {@html content.nonprofitSection}
   </section>
 </section>
