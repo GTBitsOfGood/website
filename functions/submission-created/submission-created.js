914363a0f89924937d6903efb3812b79286a9edc
Max Karpawich <max.karpawich@gmail.com>
2020-01-12 12:30:48 -0500

    remove exclamation point for professionalism in replies

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -22,7 +22,7 @@ exports.handler = async event => {
       from: 'hello@bitsofgood.org',
       to: 'hello@bitsofgood.org',
       replyTo: `${email}`,
-      subject: `Contact Us inquiry from ${name}!`,
+      subject: `Contact Us inquiry from ${name}`,
       text: `${message}`,
     })
     return {
