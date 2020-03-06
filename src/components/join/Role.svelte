b3e40db4cbb87e4b1c7b6a55d4ffc459ab6f2a78
Bits of Good <59264651+bitsofgood@users.noreply.github.com>
2020-03-06 14:30:15 -0500

    Restructure "Get Involved" for the new Contentful setup (#40)
    * restructure for new "recruitment cycle" contentful obj
    
    * remove console.log
    
    Co-authored-by: Holben888 <holben888@gmail.com>

---
+++ src/components/join/Role.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let learnMoreHash = ''
+  export let applyLink = ''
+  export let title = ''
+  export let description = ''
+</script>
+
+<style>
+  div.container,
+  div.container:hover {
+    color: #333;
+  }
+
+  div.container {
+    display: block;
+    margin: 40px 0;
+  }
+
+  div.container h4 {
+    font-family: 'Interstate';
+    font-weight: bold;
+    font-size: 22px;
+    margin: 0;
+  }
+
+  div.container p {
+    margin: 8px 0;
+  }
+</style>
+
+<div class="container">
+  <h4>{title}</h4>
+  <p>{description}</p>
+</div>
