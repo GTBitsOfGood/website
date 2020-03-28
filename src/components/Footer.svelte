4358de66889da95734c33614ec9558eedc97ca9f
Holben888 <holben888@gmail.com>
2020-03-28 16:28:21 -0400

    use regex to group contentful keys, refactor styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,20 +1,32 @@
 <script>
   import content from '@contentful-entry/footer'
+
+  const linkColumns = Object.keys(content).reduce((columns, contentKey) => {
+    const regexp = /([A-Za-z]*)Column([0-9]*)/g
+    const columnKey = regexp.exec(contentKey) // ex. matches Contentful key "linksColumn1"
+    if (columnKey) {
+      const [_, key, index] = columnKey
+      columns[index - 1] = {
+        ...columns[index - 1],
+        [key]: content[contentKey],
+      }
+    }
+    return columns
+  }, [])
 </script>
 
 <style>
-  
   footer {
     max-width: 100vw;
-    padding: 50px 3rem;
-    background-color: #FAFAFC;
+    padding: 50px 30px;
+    background-color: #fafafc;
+    margin-top: 50px;
   }
 
   section {
     display: flex;
     flex-flow: row wrap;
     margin: 50px auto;
-    max-width: 124rem;
     justify-content: space-between;
   }
 
@@ -23,23 +35,27 @@
     padding: 0;
     margin: 0;
   }
+
   h3 {
     font-size: 42px;
     padding: 0;
     margin: 0;
   }
 
-  ul {
-    display: flex;
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
+  h3.column-header {
+    font-size: inherit;
+    font-family: inherit;
   }
 
-  li {
-    display: block;
+  ul {
+    list-style: none;
+    padding: 0;
     font-size: 18px;
-    margin-bottom: 2rem;
+  }
+
+  li,
+  h3.column-header {
+    margin-bottom: 20px;
   }
 
   a {
@@ -56,78 +72,38 @@
     display: flex;
     flex-flow: row wrap-reverse;
     margin: 0 auto;
-    max-width: 124rem;
+    padding-top: 20px;
+    max-width: var(--content-max-width);
     justify-content: space-between;
+    border-top: 2px solid #727474;
   }
 
-  .left {
-    flex: 1;
-    max-width: 40rem;
-    margin: 0 0;
-    padding: 0 0;
+  .footer-bottom img {
+    margin: 10px;
+    max-width: 100%;
   }
 
-  .right {
-    display: flex;
-    flex-flow: row wrap;
-    flex-basis: 50rem;
-    justify-content: space-between;
-    margin: 0 0;
-    padding: 0 0;
+  .cta-container {
+    max-width: 400px;
+    margin-bottom: 35px;
   }
 
-  .footer_cta {
+  .link-container {
+    width: 100%;
+    max-width: 560px;
+    display: grid;
+    grid-gap: 40px;
+    grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+  }
+
+  .footer-cta {
     margin-top: 18px;
-    color: #FB3552;
+    color: var(--primary-red);
   }
 
-  .bold_link {
-    font-weight: bold;
-  }
-
-  .divider {
-    margin: 20px auto;
-    max-width: 124rem;
-    color: #727474;
-  }
-
-  .footer-img {
-    margin: 8px 0;
-    cursor: pointer;
-  }
-
-  @media (max-width: 1024px) {
+  @media (max-width: 600px) {
     footer {
-      padding: 50px 3rem;
-    }
-
-    section {
-      display: flex;
-      flex-direction: column;
-    }
-
-    .left {
-      margin-bottom: 35px;
-    }
-
-    ul {
-      margin-right: 10rem;
-      margin-bottom: 25px;
-    }
-
-    ul.last {
-      margin-right: 0;
-    }
-
-    .right {
-      justify-content: flex-start;
-      flex-basis: 20rem;
-    }
-  }
-
-  @media (max-width: 620px) {
-    footer {
-      padding: 30px 3rem;
+      padding: 30px 30px;
     }
 
     h3 {
@@ -137,45 +113,26 @@
     p {
       font-size: 26px;
     }
-
-    ul, ul.last {
-      margin-left: auto;
-      margin-right: auto;
-      margin-bottom: 25px;
-    }
-
-    li {
-      text-align: center;
-    }
-
-    .right {
-      display: flex;
-      flex-direction: column;
-    }
   }
-
 </style>
 
 <footer id="footer">
-  
+
   <section>
-    <div class="left">
-      <h3>{content.heading}</h3>
-      <p class="footer_cta">
-        <a href="/contact">{content.cta}</a>
+    <div class="cta-container">
+      <h3>{content.ctaHeader}</h3>
+      <p class="footer-cta">
+        <a href={content.cta.url}>{content.cta.text}</a>
       </p>
     </div>
 
-    <div class="right">
-      {#each content.columns as column, i}
-        <ul class:last="{i === content.columns.length - 1}">
+    <div class="link-container">
+      {#each linkColumns as column}
+        <ul>
+          <h3 class="column-header">{column.title}</h3>
           {#each column.links as link}
-            <li class:bold_link={link.boldLink}>
-              {#if link.url}
-                <a href={link.url}>{link.text}</a>
-              {:else}
-                {link.text}
-              {/if}
+            <li>
+              <a href={link.url}>{link.text}</a>
             </li>
           {/each}
         </ul>
@@ -183,13 +140,16 @@
     </div>
   </section>
 
-  <hr class="divider">
-
   <div class="footer-bottom">
-    {#each content.images as img}
-      <a href={img.link}>
-        <img src={img.image.src} alt={img.image.alt} class="footer-img">
-      </a>
-    {/each}
+    <a href={content.hack4impactLink.url}>
+      <img
+        src={content.hack4impactLink.image.src}
+        alt={content.hack4impactLink.image.alt} />
+    </a>
+    <a href={content.bitsOfGoodHomeLink.url}>
+      <img
+        src={content.bitsOfGoodHomeLink.image.src}
+        alt={content.bitsOfGoodHomeLink.image.alt} />
+    </a>
   </div>
 </footer>
