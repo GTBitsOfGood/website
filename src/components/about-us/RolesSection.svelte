ce0a380d43da5ae7fa8e3eb12ce37bbe1b20f168
Holben888 <holben888@gmail.com>
2020-03-29 18:38:08 -0400

    fix section margins

--- src/components/about-us/RolesSection.svelte
+++ src/components/about-us/RolesSection.svelte
@@ -4,22 +4,15 @@
 </script>
 
 <style>
-  .backing {
-    background: var(--backing-panel);
-    padding-bottom: 5rem;
-    overflow: hidden;
-  }
-
   section {
-    margin-top: 50px;
-    padding-bottom: 50px;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: stretch;
   }
 
-  h2, h3 {
+  h2,
+  h3 {
     text-align: center;
   }
 
@@ -33,12 +26,13 @@
   }
 </style>
 
-<div class="backing">
-  <h2>Roles</h2>
-  <h3>Bits of Good is home to engineers and artists. We believe in empowering the non-profits and democratizing technology for social good.</h3>
-  <section>
-    {#each roleDepartments as roleDepartment}
-      <Role {...roleDepartment}/>
-    {/each}
-  </section>
-</div>
+<h2>Roles</h2>
+<h3>
+  Bits of Good is home to engineers and artists. We believe in empowering the
+  non-profits and democratizing technology for social good.
+</h3>
+<section>
+  {#each roleDepartments as roleDepartment}
+    <Role {...roleDepartment} />
+  {/each}
+</section>
