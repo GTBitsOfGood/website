df8a3b09e708023047f43c63847b20e3090ea103
Holben888 <holben888@gmail.com>
2020-04-09 22:30:01 -0400

    basic dummy data to get started on details page

---
+++ src/projectDummyData.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'MapScout',
+  briefDescription: 'A visualization tool for finding nearby Trauma centers along with pertinent information.',
+  description: `Project description (5 sentences max). Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+  nonprofitName: 'PACTS',
+  nonprofitDescription: `Project description (5 sentences max). Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+}
