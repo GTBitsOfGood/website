c6ea3869a07a7dec3ab7d044930e88fff5698fe5
Holben888 <holben888@gmail.com>
2020-04-14 00:47:59 -0400

    create progress bar for project landing
    Co-authored-by: Rishov Sarkar <rishov.sarkar@gmail.com>

--- src/routes/projects/[key].svelte
+++ src/routes/projects/[key].svelte
@@ -18,15 +18,37 @@
   import KeyFeatures from '../../components/projects/KeyFeatures'
   import Reflections from '../../components/projects/Reflections'
   import AboutProject from '../../components/projects/AboutProject'
+  import { onMount } from 'svelte'
   export let content
+
+  let mounted = false
+  onMount(() => (mounted = true))
+
+  let scrollY = 0
 </script>
 
 <style>
+  .progress-bar-container {
+    background: #eee;
+    top: var(--nav-height);
+    position: sticky;
+    z-index: 9;
+  }
+  .progress-bar {
+    background: linear-gradient(
+      90deg,
+      var(--primary-red),
+      var(--primary-yellow)
+    );
+    height: 10px;
+  }
   .projects-container {
-    padding: 30px;
+    position: relative;
     --content-max-width: 1000px;
   }
   .projects-container :global(section) {
+    padding-left: 30px;
+    padding-right: 30px;
     margin-top: 80px;
     margin-bottom: 80px;
   }
@@ -41,8 +63,9 @@
     font-size: 30px;
   }
   @media (max-width: 600px) {
-    .projects-container {
-      padding: 20px;
+    .projects-container :global(section) {
+      padding-left: 20px;
+      padding-right: 20px;
     }
     .projects-container :global(h2) {
       font-size: 18px;
@@ -54,8 +77,17 @@
   }
 </style>
 
+<svelte:window bind:scrollY />
+
 <Header {content} />
 <div class="projects-container">
+  <div class="progress-bar-container">
+    {#if mounted}
+      <div
+        class="progress-bar"
+        style={`width: ${(scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100}%`} />
+    {/if}
+  </div>
   <AboutProject {...content} />
   <KeyFeatures {...content} />
   <Reflections {...content} />
