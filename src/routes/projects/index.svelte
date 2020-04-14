3c70fa902e4fb4d5ea63c84fbc497eed96386122
Holben888 <holben888@gmail.com>
2020-04-14 17:51:04 -0400

    move projects section into routes file, USE GRID!

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,12 +1,52 @@
 <script>
   import PageHeader from '../../components/PageHeader'
-  import ProjectSection from '../../components/projects/index/ProjectSection'
+  import Project from '../../components/projects/index/Project'
 
-  import headerContent from '@contentful-entry/projectLanding'
+  import content from '@contentful-entry/projectLanding'
 </script>
 
-<PageHeader {...headerContent} />
-<ProjectSection />
+<style>
+  section {
+    padding: 30px;
+  }
+
+  .projects-container {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 340px);
+    column-gap: 60px;
+    row-gap: 100px;
+    justify-content: start;
+    align-items: stretch;
+  }
+
+  @media (max-width: 800px) {
+    section {
+      padding: 20px;
+    }
+    .projects-container {
+      justify-content: center;
+    }
+
+    h2 {
+      text-align: center;
+    }
+  }
+</style>
+
+<PageHeader {...content} />
+<section>
+  <h2>Current Projects</h2>
+  <div class="projects-container">
+    {#each content.projects as { name, briefDescription, key, thumbnail }}
+      <Project
+        {name}
+        link={'/projects/' + key}
+        image={thumbnail}
+        {briefDescription} />
+    {/each}
+  </div>
+</section>
 
 <svelte:head>
   <title>Our Projects</title>
